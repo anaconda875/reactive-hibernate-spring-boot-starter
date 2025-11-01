af9466e73e6e06ed56ab27955a21c30931aaaa23
Paul Ngo (Do Not Bug) <hflbtmax@gmail.com>
2025-11-01 14:58:54 +0700

    pump spring to 3.3.2 (#14)
    * pump spring to 3.3.2
    
    * pump version
    
    ---------
    
    Co-authored-by: Bao.Ngo <bao.ngo@aeris.net>

--- src/main/java/com/htech/data/jpa/reactive/repository/query/ReactiveJpaParameters.java
+++ src/main/java/com/htech/data/jpa/reactive/repository/query/ReactiveJpaParameters.java
@@ -1,30 +1,28 @@
 package com.htech.data.jpa.reactive.repository.query;
 
 import jakarta.persistence.TemporalType;
-import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.List;
 import org.springframework.core.MethodParameter;
 import org.springframework.data.jpa.repository.Temporal;
 import org.springframework.data.repository.query.Parameter;
 import org.springframework.data.repository.query.Parameters;
+import org.springframework.data.repository.query.ParametersSource;
 import org.springframework.data.util.TypeInformation;
 import org.springframework.lang.Nullable;
 
 public class ReactiveJpaParameters
     extends Parameters<ReactiveJpaParameters, ReactiveJpaParameters.JpaParameter> {
 
-  public ReactiveJpaParameters(Method method) {
-    super(method, null);
+  public ReactiveJpaParameters(ParametersSource parametersSource) {
+    super(
+        parametersSource,
+        methodParameter ->
+            new JpaParameter(methodParameter, parametersSource.getDomainTypeInformation()));
   }
 
   private ReactiveJpaParameters(List<ReactiveJpaParameters.JpaParameter> parameters) {
     super(parameters);
-  }
-
-  @Override
-  protected ReactiveJpaParameters.JpaParameter createParameter(MethodParameter parameter) {
-    return new ReactiveJpaParameters.JpaParameter(parameter);
   }
 
   @Override
@@ -41,12 +39,10 @@ public class ReactiveJpaParameters
     private final @Nullable Temporal annotation;
     private @Nullable TemporalType temporalType;
 
-    protected JpaParameter(MethodParameter parameter) {
-      super(parameter, TypeInformation.of(Parameter.class));
-
+    protected JpaParameter(MethodParameter parameter, TypeInformation<?> domainType) {
+      super(parameter, domainType);
       this.annotation = parameter.getParameterAnnotation(Temporal.class);
       this.temporalType = null;
-
       if (!isDateParameter() && hasTemporalParamAnnotation()) {
         throw new IllegalArgumentException(
             Temporal.class.getSimpleName() + " annotation is only allowed on Date parameter");
